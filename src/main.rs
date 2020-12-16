12751f16182369baa985c9c1a934e1f465cae3d1
Nbiba Bedis <bedisnbiba@gmail.com>
2020-12-16 19:01:13 +0100

    use sqlite as database

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 #![allow(clippy::many_single_char_names)]
 use gtk::*;
 use serde::Deserialize;
-
-const URL: &str = "http://91.132.145.114/json/stations/search?";
+use std::io::Write;
 
 macro_rules! shadow_clone {
     ($($i:ident),+) => ($(let $i = $i.clone();)+)
 }
 
+#[derive(Debug)]
+enum Tag {
+    Country(String),
+    Name(String),
+}
+
 fn main() {
     gtk::init().unwrap();
 
-    //ureq
-    let mut client = ureq::agent();
-    client.set(
-        "User-Agent",
-        "nanowave(https://github.com/sigmaSd/nanowave)",
-    );
+    let path = dirs_next::cache_dir().unwrap().join("nanowave");
+    let connection = if path.join("db.sqlite").exists() {
+        sqlite::open(path.join("db.sqlite")).unwrap()
+    } else {
+        create_db(&path)
+    };
 
     // Main window
     let w = Window::new(WindowType::Toplevel);
@@ -36,19 +41,47 @@ fn main() {
             let text = entry.get_text().to_string();
             let text = text.trim();
 
-            let url = if text.contains("c:") {
-                let country = text.split("c:").nth(1).unwrap();
-                let mut country = country.chars();
-                let mut c = country.next().unwrap().to_uppercase().to_string();
-                let cc: String = country.collect();
-                c.push_str(&cc);
+            let v: Vec<_> = text
+                .split(',')
+                .map(|s| {
+                    let mut s = s.split(':');
+                    let tag = s.next().unwrap();
+                    let val = s.next().unwrap();
+                    match tag {
+                        "c" => Tag::Country(val.to_string()),
+                        "s" => Tag::Name(val.to_string()),
+                        _ => unreachable!(),
+                    }
+                })
+                .collect();
 
-                format!("{}country={}", URL, c)
-            } else {
-                format!("{}name={}", URL, text)
-            };
+            let mut query = "SELECT * from mytable ".to_string();
+            for (i, tag) in v.into_iter().enumerate() {
+                match i {
+                    0 => query.push_str("WHERE "),
+                    _ => query.push_str("AND "),
+                }
+                match tag {
+                    Tag::Name(name) => {
+                        query.push_str(&format!("Name LIKE \"%{}%\"", name));
+                    }
+                    Tag::Country(country) => {
+                        query.push_str(&format!("Country LIKE \"%{}%\"", country));
+                    }
+                }
+            }
 
-            let stations: Vec<Station> = client.get(&url).call().into_json_deserialize().unwrap();
+            let mut stations = vec![];
+            connection
+                .iterate(query, |cols| {
+                    let mut cols = cols.iter();
+                    let name = cols.next().unwrap().1.unwrap().to_string();
+                    let country = cols.next().unwrap().1.unwrap().to_string();
+                    let url = cols.next().unwrap().1.unwrap().to_string();
+                    stations.push(Station { name, country, url });
+                    true
+                })
+                .unwrap();
 
             let width = 5;
             let mut pos = (0, 0);
@@ -105,9 +138,54 @@ fn main() {
 
     gtk::main();
 }
+fn create_db(path: &std::path::Path) -> sqlite::Connection {
+    println!("Updating database..");
+
+    let mut client = ureq::agent();
+    client.set(
+        "User-Agent",
+        "nanowave(https://github.com/sigmaSd/nanowave)",
+    );
+    let s: Vec<Station> = client
+        .get("http://91.132.145.114/json/stations")
+        .call()
+        .into_json_deserialize()
+        .unwrap();
+
+    let _ = std::fs::create_dir_all(&path);
+    let _ = std::fs::remove_file(path.join("db.sqlite"));
+    let _ = std::fs::remove_file(path.join("db.csv"));
+
+    let mut o = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path.join("./db.csv"))
+        .unwrap();
+    writeln!(o, "Name,Country,Url").unwrap();
+    for ss in s {
+        writeln!(o, "{},{},{}", ss.name, ss.country, ss.url).unwrap();
+    }
+
+    let mut p = std::process::Command::new("sqlite3")
+        .current_dir(path)
+        .stdin(std::process::Stdio::piped())
+        .arg("db.sqlite")
+        .spawn()
+        .unwrap();
+    writeln!(
+        p.stdin.as_mut().unwrap(),
+        ".mode csv\n.import db.csv mytable"
+    )
+    .unwrap();
+    p.wait().unwrap();
+
+    println!("Done!");
+    sqlite::open(path.join("db.sqlite")).unwrap()
+}
 
 #[derive(Debug, Deserialize)]
 struct Station {
     name: String,
     url: String,
+    country: String,
 }
